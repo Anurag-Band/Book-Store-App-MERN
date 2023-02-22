b88059b982a0faf5d1209bf45e1df9deac07d81d
Anurag Band <anuragband10@gmail.com>
2023-02-22 23:46:29 +0530

    bug fixes - carts page

--- client/src/components/cart/CartItemCard.jsx
+++ client/src/components/cart/CartItemCard.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CartItemCard = ({ item }) => {
+  if (!item?.itemId?._id) return;
   return (
     <>
       <Link
-        to={`/product/${item.itemId?._id}`}
+        to={`/product/${item?.itemId?._id}`}
         className="w-40 h-40 overflow-hidden rounded-full"
       >
         <img
-          src={item.itemId?.photos[0].secure_url}
-          alt={item.name}
+          src={item?.itemId?.photos[0].secure_url}
+          alt={item?.name}
           className="w-40 h-40 object-cover"
         />
       </Link>
       <h3 className="text-md text-slate-500 font-medium line-clamp-2 w-[40%] px-2">
-        {item.name}
+        {item?.name}
       </h3>
     </>
   );
